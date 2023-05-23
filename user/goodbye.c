259affd6f0ea64184537cfdb4697c7bca41921f9
yoavital <yoavital98@gmail.com>
2023-05-24 00:20:00 +0300

    uploaded first 2 tasks

--- user/goodbye.c
+++
@@ -1,6 +0,0 @@
-#include "user.h"
-#include "kernel/types.h"
-
-int main(){
-    exit(0, "Goodbye World xv6\n");
-}
--- user/helloworld.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello World xv6\n");
-    return 0;
-}
